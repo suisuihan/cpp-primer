384129d66bd2d76675d1ac9b76a82a3f2dde2cf2
suisuihan <fengbobao@163.com>
2016-07-09 06:22:16 -0700

    add ex2_32

---
+++ chapter2/ex2_29.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+	const int ic=0;
+	const int *const p3=&ic;
+}
---
+++ chapter2/ex2_32.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;
+
+int main(int argc, char** argv){
+	int null = 0, *p = nullptr;
+}

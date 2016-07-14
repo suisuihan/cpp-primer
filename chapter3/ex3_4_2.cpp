87e25472795067f17e8f62571974f1e0db4f421b
suisuihan <fengbobao@163.com>
2016-07-14 08:59:20 -0700

    add to ex3_5

---
+++ chapter3/ex3_4_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char** argv){
+    string concatstring;
+    string bufferstring;
+    while(cin >> bufferstring){
+        concatstring+=bufferstring+' ';
+    }
+    cin.clear();
+    cout << "the concatstring is: " << concatstring << endl;
+}
